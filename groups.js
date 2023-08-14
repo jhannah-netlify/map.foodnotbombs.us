668e9ca5fe23918c02c6c3bece636831a7e6f6b0
Jay Hannah <jay.hannah@iinteractive.com>
2023-08-14 18:28:14 -0500

    whitespace / tab cleanup

--- groups.js
+++ groups.js
@@ -1,31 +1,30 @@
 // Javascript representation of a GeoJSON file
 //   https://wiki.openstreetmap.org/wiki/GeoJSON
 var fnbGroups = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [-95.934398, 41.257502]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Omaha: Food distribution every Sunday at 2pm. Corner of 14th and Farnum.",
-		"website": "http://omaha.foodnotbombs.us",
-            },
-            "id": 1
-        },
-        {
-            "geometry": {
-                "type": "Point",
-                "coordinates": [ -104.9963919, 39.7444271 ]
-            },
-            "type": "Feature",
-            "properties": {
-                "popupContent": "Just a place-holder. Ignore me."
-            },
-            "id": 2
-        },
-    ]
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-95.934398, 41.257502]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "Omaha: Food distribution every Sunday at 2pm. Corner of 14th and Farnum.",
+				"website": "http://omaha.foodnotbombs.us",
+			},
+			"id": 1
+		},
+		{
+			"geometry": {
+				"type": "Point",
+				"coordinates": [ -104.9963919, 39.7444271 ]
+			},
+			"type": "Feature",
+			"properties": {
+				"popupContent": "Just a place-holder. Ignore me."
+			},
+			"id": 2
+		},
+	]
 };
-
